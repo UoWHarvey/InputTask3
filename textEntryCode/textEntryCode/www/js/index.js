274f5e87553e938e869fd0aa03b04979aa5c58cc
UoWHarvey <55839084+UoWHarvey@users.noreply.github.com>
2019-10-31 16:53:31 +0000

    Update index.js

--- textEntryCode/textEntryCode/www/js/index.js
+++ textEntryCode/textEntryCode/www/js/index.js
@@ -6,10 +6,64 @@ $(document).on("pagecreate","#pageone",function(){
 
 
 function submitText() {
-	var text = $('#textinput').val();
-	alert(text);
+	var Name = $('#Name','#Email','#Phone Number','#Dateofbirth','#Password'.val();
+	alert(Name);
+    alert(Email);
+    alert(Phone Number);
+    alert(Dateofbirth);
+    alert(Password);
+    
+    storeValue('alert', Name);
 }
 
 function storeValue(key, value) {
+    localStorage.setItem (key,value);
 	//add some code to store the key-value pair in persistant storage 
 }
+var paused_count =0;
+var resumed_count = 0;
+var launched_count = 0;
+
+document.addEventListener("deviceready", onDeviceReady, false);
+		
+	
+function updateDisplay() {
+	$("#launched").text("Application launched: " + launched_count);
+	$("#resumed").text("Application paused: " + paused_count);
+	$("#paused").text("Application resumed: " + resumed_count);
+}
+
+
+// device APIs are available
+//
+    function onDeviceReady() {
+	
+	document.addEventListener("resume", onResume, false);
+	document.addEventListener("pause", onPause, false);
+	
+	launched_count++;
+	updateDisplay();
+	    
+	alert("device ready");
+    }
+
+
+    function onPause() {
+	
+	paused_count++;
+	updateDisplay();
+	    
+	alert("pause");
+    }
+	
+
+    function onResume() {
+		
+	resumed_count++;
+	updateDisplay();
+	    
+	alert("resume");
+    }
+
+
+});
